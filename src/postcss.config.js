ab6b4176300bbeb1e822da1ed77b85b3b34313dd
Kiril Mandov <kiril@morphocode.com>
2016-03-10 22:42:55 +0200

    Basscss skeleton page

---
+++ src/postcss.config.js
@@ -0,0 +1,17 @@
+var postcss = require('postcss');
+
+module.exports = {
+  use: [
+    'postcss-import',
+    'postcss-custom-media',
+    'postcss-custom-properties',
+    'postcss-calc',
+    'postcss-color-function',
+    'postcss-discard-comments',
+    'autoprefixer'
+  ],
+  input: 'src/theme.css',
+  dir: 'css/'
+};
+
+
